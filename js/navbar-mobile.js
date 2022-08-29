c2e77b4f7fdc3e580d0dc772f80d55dd6e44f850
Wesley Ricardo <10687923@tkatm.local>
2022-08-29 14:55:17 -0300

    projeto tcc pagina inicial

---
+++ js/navbar-mobile.js
@@ -0,0 +1,18 @@
+const btnMobile = document.getElementById('btn'); //declara a constante como sendo o botao
+function expandirMenu(event){
+    if(event.type === 'touchstart') event.preventDefault() //para touchs de celulares
+    const nav = document.getElementById('nav');
+    nav.classList.toggle('active'); //da o atributo de ativado para o nav do site
+}
+btnMobile.addEventListener('click', expandirMenu) // adiciona o evento a função com click
+btnMobile.addEventListener('touchstart', expandirMenu) // mesma coisa da de cima para celular
+
+
+
+//-=-=-=-=-=-=-faz a mesma coisa que o de cima so que com o ling para aticar o submenu
+const ativarMenu = document.getElementById('ling')
+function expandirSubmenu(){
+    const submenu = document.getElementById('ling')
+    submenu.classList.toggle('active')
+}
+ativarMenu.addEventListener('click', expandirSubmenu)
