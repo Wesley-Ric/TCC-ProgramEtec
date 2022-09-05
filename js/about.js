c438c94493580331607164780496e7bbf0b94ded
Wesley Ricardo <wesleyric.git@gmail.com>
2022-09-05 16:32:24 -0300

    comentarios no codigo

--- js/about.js
+++ js/about.js
@@ -1,17 +1,16 @@
-const target = document.querySelectorAll('[data-anime]');
-const classeAnime = 'animate';
+const target = document.querySelectorAll('[data-anime]');//seleciona todos que estão com data-anime
+const classeAnime = 'animate';//da o valor de animate para classAnime
 
 
 function animeScroll (){
-    const topJanela = window.pageYOffset + ((window.innerHeight * 3) / 4);
+    const topJanela = window.pageYOffset + ((window.innerHeight * 3) / 4);//seta onde a animação vai começar
     target.forEach(function(element){
         if ((topJanela) > element.offsetTop) {
-            element.classList.add(classeAnime);
+            element.classList.add(classeAnime);//se o elemento estiver no topo da janela ele da o status de animate pata elemento
         }
-        console.log(element.offsetTop);
     })
 }
 
 window.addEventListener('scroll', function(){
     animeScroll();
-})
+})//chama o evento de scroll para função
