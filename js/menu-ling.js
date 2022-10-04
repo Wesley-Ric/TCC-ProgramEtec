6d2e15a09ad405d3cd9839a69d864974a2433a65
Wesley Ricardo <wesleyric.git@gmail.com>
2022-10-04 16:05:55 -0300

    atualizações mobile

--- js/menu-ling.js
+++ js/menu-ling.js
@@ -1,8 +1,18 @@
-const btnMobile = document.getElementById('botaozinho'); //declara a constante como sendo o botao
-function expandirMenu(event){
+const btnmen = document.getElementById('botaozinho'); //declara a constante como sendo o botao
+function menuExp(event){
     if(event.type === 'touchstart') event.preventDefault() //para touchs de celulares
-    const menu = document.getElementById('navgation');
-    menu.classList.toggle('active'); //da o atributo de ativado para o nav do site
+    const menuzin = document.getElementById('navgation');
+    menuzin.classList.toggle('active'); //da o atributo de ativado para o nav do site
 }
-btnMobile.addEventListener('click', expandirMenu) // adiciona o evento a função com click
-btnMobile.addEventListener('touchstart', expandirMenu) // mesma coisa da de cima para celular
+btnmen.addEventListener('click', menuExp) // adiciona o evento a função com click
+btnmen.addEventListener('touchstart', menuExp) // mesma coisa da de cima para celular
+
+
+const btn2 = document.getElementById('btn2'); //declara a constante como sendo o botao
+function menuEx(event){
+    if(event.type === 'touchstart') event.preventDefault() //para touchs de celulares
+    const menu2 = document.getElementById('navgation');
+    menu2.classList.toggle('active'); //da o atributo de ativado para o nav do site
+}
+btn2.addEventListener('click', menuEx) // adiciona o evento a função com click
+btn2.addEventListener('touchstart', menuEx) // mesma coisa da de cima para celular
