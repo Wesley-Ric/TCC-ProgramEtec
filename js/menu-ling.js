d6c4119954030d4ea9fb92faff1b26e72a3c48b7
Matheus Mendes <matheus271005@gmail.com>
2022-11-01 17:40:21 -0300

    segunda-melhoria-js

--- js/menu-ling.js
+++ js/menu-ling.js
@@ -1,6 +1,6 @@
 const btnmen = document.getElementById('botaozinho'); //declara a constante como sendo o botao
-function menuExp(event){
-    if(event.type === 'touchstart') event.preventDefault() //para touchs de celulares
+function menuExp(event) {
+    if (event.type === 'touchstart') event.preventDefault() //para touchs de celulares
     const menuzin = document.getElementById('navgation');
     menuzin.classList.toggle('active'); //da o atributo de ativado para o nav do site
 }
@@ -9,8 +9,8 @@ btnmen.addEventListener('touchstart', menuExp) // mesma coisa da de cima para ce
 
 
 const btn2 = document.getElementById('btn2'); //declara a constante como sendo o botao
-function menuEx(event){
-    if(event.type === 'touchstart') event.preventDefault() //para touchs de celulares
+function menuEx(event) {
+    if (event.type === 'touchstart') event.preventDefault() //para touchs de celulares
     const menu2 = document.getElementById('navgation');
     menu2.classList.toggle('active'); //da o atributo de ativado para o nav do site
 }
--- js/navbar-mobile.js
+++ js/navbar-mobile.js
@@ -1,6 +1,6 @@
 const btnMobile = document.getElementById('btn'); //declara a constante como sendo o botao
-function expandirMenu(event){
-    if(event.type === 'touchstart') event.preventDefault() //para touchs de celulares
+function expandirMenu(event) {
+    if (event.type === 'touchstart') event.preventDefault() //para touchs de celulares
     const nav = document.getElementById('nav');
     nav.classList.toggle('active'); //da o atributo de ativado para o nav do site
 }
@@ -11,7 +11,8 @@ btnMobile.addEventListener('touchstart', expandirMenu) // mesma coisa da de cima
 
 //-=-=-=-=-=-=-faz a mesma coisa que o de cima so que com o ling para aticar o submenu
 const ativarMenu = document.getElementById('ling')
-function expandirSubmenu(){
+
+function expandirSubmenu() {
     const submenu = document.getElementById('ling')
     submenu.classList.toggle('active')
 }
