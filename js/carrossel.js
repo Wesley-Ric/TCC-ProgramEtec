1b3df5eae0070bbd7677dff49ffc7cdd9697b1cb
Wesley Ricardo <wesleyric.git@gmail.com>
2022-11-01 16:50:44 -0300

    correções

--- js/carrossel.js
+++ js/carrossel.js
@@ -1,14 +1,14 @@
 let cont = 1; //seta o contador do tipo let como 1
 document.getElementById("radio1").checked = true; //o radio esta checado
-setInterval(function(){
+setInterval(function () {
     proximaImagem();
 }, 3500) //intervalo de 3500 milisegundos
 
-function proximaImagem(){
+function proximaImagem() {
     cont++; //soma 1 ao contador
-    if(cont>5){
-        cont=1; //se o contador for maior que 5 reseta pra 1
-                //isso faz com que a imagem volte para primeira
+    if (cont > 5) {
+        cont = 1; //se o contador for maior que 5 reseta pra 1
+        //isso faz com que a imagem volte para primeira
     }
-    document.getElementById("radio"+cont).checked = true; //deixa checado de acordo com o número do contador
+    document.getElementById("radio" + cont).checked = true; //deixa checado de acordo com o número do contador
 }
---
+++ readme.md
@@ -0,0 +1,4 @@
+# Trabalho de conclusão de curso
+## programEtec
+
+Nosso projeto foi feito com o objetivo de levar ainda mais conhecimento aos alunos da Etec
